2ca4d6bcae818b78a45985144f532268932b3b1d
Adam Basiak <adam.basiak01@gmail.com>
2022-06-12 10:33:48 +0200

    commit test

--- src/SpecifiedClasses/LoginProcess.java
+++ src/SpecifiedClasses/LoginProcess.java
@@ -1,10 +1,20 @@
 package SpecifiedClasses;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginProcess {
-    public int login(Connection connection,String username,String password){
+    public int login(Connection connection, String username, String password) throws SQLException {
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(
+                "SELECT *\n" +
+                        "FROM nauczyciele\n" +
+                        "ORDER BY id_nauczyciela")) {
+            while (resultSet.next()) {
 
-        return 0;
+            }
+            return 0;
+        }
     }
 }
