de929472f38021fa578ebcbeacd72b113e886a89
Adam Basiak <adam.basiak01@gmail.com>
2022-06-12 10:28:49 +0200

    Initial commit

---
+++ src/SpecifiedClasses/LoginProcess.java
@@ -0,0 +1,10 @@
+package SpecifiedClasses;
+
+import java.sql.Connection;
+
+public class LoginProcess {
+    public int login(Connection connection,String username,String password){
+
+        return 0;
+    }
+}
